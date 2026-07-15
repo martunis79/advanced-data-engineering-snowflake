5403b9dc57aefae23fabaffbbb19435f54b3e8c3
martunis79 <81718544+martunis79@users.noreply.github.com>
2026-07-15 17:08:45 +0100

    add file to mod01 folder

---
+++ _my-code/Mod01-devops/01-devops.sql
@@ -0,0 +1,25 @@
+USE ROLE accountadmin;
+CREATE DATABASE course_repo;
+USE SCHEMA public;
+
+-- Create credentials
+CREATE OR REPLACE SECRET course_repo.public.github_pat
+  TYPE = password
+  USERNAME = 'martunis79'
+  PASSWORD = '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';
+
+-- Create the API integration
+CREATE OR REPLACE API INTEGRATION git_api_integration
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/martunis79') -- URL to your GitHub profile
+  ALLOWED_AUTHENTICATION_SECRETS = (github_pat)
+  ENABLED = TRUE;
+
+-- Create the git repository object
+CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
+  API_INTEGRATION = git_api_integration -- Name of the API integration defined above
+  ORIGIN = 'https://github.com/martunis79/advanced-data-engineering-snowflake.git' -- Insert URL of forked repo
+  GIT_CREDENTIALS = course_repo.public.github_pat;
+
+-- List the git repositories
+SHOW GIT REPOSITO RIES;
